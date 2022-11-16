c3a4b0f6f8b4c9dd3553ed6f7a764ce9c64b4927
Xyao2001 <65087376+Xyao2001@users.noreply.github.com>
2022-11-16 17:14:48 +0000

    FULLY WORKING CODE

--- mcwtest.py
+++ mcwtest.py
@@ -1,3 +1,4 @@
+# Online Python compiler (interpreter) to run Python online.
 import numpy as np
 import pandas as pd
 import random
@@ -15,11 +16,11 @@ class MC:
     global h_bar
     h_bar=1
     global initial_state
-    initial_state = np.array([1,0]) 
+    initial_state = np.array([[1],[0]]) 
     global H_eff
 
     # H_eff=np.array([[delta,omega],[omega,-1*delta-(1j*gamma)]])
-    H_eff=np.array([[0,600],[600,-(1j*300)]])
+    H_eff=np.array([[0,0.1],[0.1,-(1j*0.01)]])
 
     #random.random() function to generate a random floating-point number between 0 and 1 in Python.
     def __init__(self,delta,omega,gamma):
@@ -40,69 +41,52 @@ class MC:
         time=time
         n=shell_number
         dt=steps
-        excited_states=[0]
+        m_1=np.array([[1,0],[0,1]])
+        excited_states=[]
         sum_wave_vector=[]
-        '''
-        # evolve the wavevector by a very small time step
-        evolved_=(1-((1j)*(H_eff*dt)))*initial_state
-        #Then, choose a random number r between 0 and and 1
-        r=random.randint(0,1)
-        print(f'r is %%%%%%%%{r}')
-        evolved_star=evolved_.conj().T
-        norm_=(np.sum(np.multiply(evolved_star,evolved_))).real
-        norm_squared=int(norm_)
-        '''
+        H_eff=np.array([[0,0.1],[0.1,-(1j*0.01)]])
         sum_time=0
-        psi=initial_state-1-(sum_time/2j*(H_eff*initial_state))
-        psi/=np.linalg.norm(psi)
-        #normalise psi
-
+        psi=initial_state
         #print(f"effective hermitian is {H_eff}  initial state is {initial_state}, h_eff on initial State is {H_eff*initial_state}")
-        while sum_time!=time: 
+        while sum_time<time: 
             sum_time+=dt
+            
             #coefficient=np.sum(1-np.sum((1j*(H_eff*sum_time))))
-            coefficient=(1-((1j*(H_eff*sum_time))))
-            evolved_=coefficient*psi
-            norm=np.linalg.norm((evolved_))
+            coefficient=m_1-(0.5j)*H_eff
+            psi=np.matmul(coefficient,psi)
+            norm=np.linalg.norm((psi))
+            
+            #norm_squared=(np.abs(np.matmul(np.conj(np.transpose(evolved_)),evolved_)))[1,1]
             norm_squared=norm*norm
-
-            #Then, choose a random number r between 0 and and 1
-            r=random.uniform(0,1)
-            #print(f"r is {r}, and norm squared is {norm_squared}")
-
+            r=random.random()
             #If r is less than the norm-squared h then there is no decay and we simply renormalise
             if (r<norm_squared):
 
                 # then there is no decay and we simply renormalise
-                psi=evolved_/np.sqrt(norm_squared)
+                psi=psi/np.sqrt(norm_squared)
                 #print(f"no jump and wave vector is {psi}")
-                sum_wave_vector.append(psi)
+                sum_wave_vector.append(abs(psi[1]**2))
 
         #if r is greater than or equal to norm _squared then there's a decay
             elif r>=norm_squared:
 
                 #we apply a jump operator
-                gamma=0.03
+                gamma=0.01
                 #print('time evo operator is being applied')
-                # je_star is the hermitian of time_evo, i know these names are awful
-                jump_operator = math.sqrt(gamma/2)*np.array([[0, 1], [0, 0]])
-                psi_0= jump_operator*evolved_
+                #je_star is the hermitian of time_evo, i know these names are awful
+                jump_operator=math.sqrt(gamma/2)*np.array([[0,1],[0,0]])
+                psi_1=np.matmul(jump_operator,psi)
                 #print(f" ::::psi is {evolved_}:the jump operator acting on psi is:::::: {psi_1}")
-                # the wave vector is giving nan values
+                #the wave vector is giving nan values
 
-                psi = (psi_0/np.sqrt(norm_squared))
-                sum_wave_vector.append(psi)
-        sum_wave_vector=(sum_wave_vector)
-        for i in range(len(sum_wave_vector)):
-            #print the excited populations
-            arr=(sum_wave_vector[i])
+                norm_square=np.linalg.norm((psi_1))*np.linalg.norm((psi_1))
+
+                psi=psi_1/np.sqrt(norm_square)
+                sum_wave_vector.append(abs(psi[1]**2))
             
-            
-            
-            excited_states.append(arr[1,1].real)
         
 
-        return excited_states
+        return sum_wave_vector
 
 
 
@@ -114,30 +98,48 @@ class MC:
 
             return density_matrix_sum
 
-    def MC_Sampling(self, n_trajectory,time):
-        num=n_trajectory
-        r=random.random()
-        excitations=[]
-        sum_traj=self.evolve(2,time)
+class OBE:
+    def __init__(self):
+        None
 
-        sum_t_c=self.transpose_conjugate(num, sum_traj)
-        density_matrix=(1/num)*sum_t_c
-        return density_matrix[3]
-        #
+    def analytical_solution(self, time,gamma, omega):
+        
+        results=[]
+        for i in range(time):
+            t=i
+            expo_term=(math.exp(-(3*gamma*t)/4))
+            term_1=(math.cos(math.sqrt(omega**2-((gamma**2)/16))*t))
+            term_2=((3*gamma)/(math.sqrt(16*(omega**2)-(gamma**2))))*math.sin(math.sqrt((omega**2)-(gamma**2)/16)*t)
+            p_11=(omega**2/(2*(omega**2)+(gamma**2)))*(1-(expo_term*(term_1+term_2)))
+            results.append([p_11])
+        
+        return results
 
-time=7.5
-steps=0.5
+obe=OBE()
+
+time=1000
+steps=1
 times=np.arange(start=0, stop=time, step=steps)
 
 Monte=MC(0,1,2)
 
-arr=np.array([1,0])
-
-time = 100
-times = np.arange(time)
 
 
+y=(Monte.evolve(2,time,steps))
+
+avg=[]
+
+for i in range(time):
+    monte=Monte.evolve(2,time,steps)
+    avg.append(monte) #list of lists
+arr=np.array(avg)
+y1 = np.average(arr, axis=0)
+
+y2=obe.analytical_solution(time,0.01,0.1)
 plt.figure()
-plt.plot(times, y)
+plt.plot(times,y, label = "single MCW")
+plt.plot(times,y1, label = "Averaged MCW")
+plt.plot(times,y2, label = "OBE")
+plt.legend()
 
 plt.show()
